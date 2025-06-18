9a72a80860437b58f1b8221d1daf78052bad4ece
Nisarg153 <nisarg150301@gmail.com>
2025-06-18 19:05:51 -0400

    Updated AMI

--- modules/ec2/scripts/web-userdata.sh
+++ modules/ec2/scripts/web-userdata.sh
@@ -4,11 +4,12 @@
 su - ec2-user <<'EOF'
 cd ~/
 rm -rf terraform-aws-multi-env-webapp
-git clone https://github.com/jatharthan/terraform-aws-multi-env-webapp
+git clone https://github.com/Nisarg153/terraform-aws-multi-env-webapp
 cd terraform-aws-multi-env-webapp/app_code
 echo "Deploying Git SHA: ${project_prefix}-${git_sha}" >> /home/ec2-user/deploy.log
 npm install express body-parser aws-sdk
-pm2 start app.js --name recovery-app
+sudo npm install -g pm2
+pm2 start app.js --name app
 pm2 startup systemd -u ec2-user --hp /home/ec2-user | tail -n 1 | sh
 pm2 save
 
